69a12bfd43bed55da267453df6681974e57df615
GPH <gph1990@live.com>
2018-07-16 02:26:55 -0300

    grid step
    feels clunky

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -10,13 +10,17 @@ public class PlayerController : NetworkBehaviour
 
     // mouse movement
     private Vector2 targetPosition;
-    enum Direction {Top,Right,Bottom, Left};
-    float stepsPerTime = 0.1f;
-    Direction facing; 
+    enum Direction {TopLeft, Top, TopRight, Right, BottomRight, Bottom, BottomLeft, Left};
+    Direction facing;
+    float timePerStep = 0.1f;
+
     // direction 
     private Vector2 heading;
     private Vector2 direction;
 
+    // healthbar direction
+
+    public Canvas healthbar;
     // turn ON fire
     public bool spawnProj;
 
@@ -44,15 +48,14 @@ public class PlayerController : NetworkBehaviour
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
         Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, new Vector3(targetPosition.x, targetPosition.y, Camera.main.transform.position.z), Time.deltaTime * 5);
         */
-
-        
+        timePerStep -= Time.deltaTime;
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            stepsPerTime -= Time.deltaTime;
-            if (stepsPerTime < 0)
+            
+            if (timePerStep < 0)
             {
                 Cmdstep();
-                stepsPerTime = 0.5f;
+                timePerStep = 0.1f;
             }
 
             /*
@@ -81,47 +84,82 @@ public class PlayerController : NetworkBehaviour
         }
 
         Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, new Vector3(targetPosition.x, targetPosition.y, Camera.main.transform.position.z), Time.deltaTime * 100);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // MOUSE DIRECTION
-
-        if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x)
-        {
-            //transform.localScale = new Vector3(-1, 1, 1);
-            if (Camera.main.ScreenToWorldPoint(Input.mousePosition).y > transform.position.y + 2)
+        // SET DIRECTION
+        if (mousePos.x > transform.position.x + 0.5) {
+            if (mousePos.y > transform.position.y + 0.5)
             {
-                projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-                facing = Direction.Top;
-            }
-            else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y - 2)
-            {
-                projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
-                facing = Direction.Bottom;
+                // TOPRIGHT
+                projectileSpawn.transform.position = new Vector3(transform.position.x + 1, transform.position.y + 1, transform.position.z);
+                facing = Direction.TopRight;
             }
             else
             {
-                projectileSpawn.transform.position = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
-                facing = Direction.Right;
+                if (mousePos.y < transform.position.y - 0.5)
+                {
+                    // BOTTOMRIGHT
+                    projectileSpawn.transform.position = new Vector3(transform.position.x + 1, transform.position.y -1, transform.position.z);
+                    facing = Direction.BottomRight;
+                }
+                else
+                {
+                    // RIGHT
+                    projectileSpawn.transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+                    facing = Direction.Right;
+                }
             }
-
         }
         else
         {
-            //transform.localScale = new Vector3(1, 1, 1);
-            if (Camera.main.ScreenToWorldPoint(Input.mousePosition).y > transform.position.y + 2)
+            if (mousePos.x < transform.position.x - 0.5)
             {
-                projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-                facing = Direction.Top;
-            }
-            else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y - 2)
-            {
-                projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
-                facing = Direction.Bottom;
+                if (mousePos.y > transform.position.y + 0.5)
+                {
+                    //TOPLEFT
+                    projectileSpawn.transform.position = new Vector3(transform.position.x - 1, transform.position.y + 1, transform.position.z);
+                    facing = Direction.TopLeft;
+                }
+                else
+                {
+                    if (mousePos.y < transform.position.y - 0.5)
+                    {
+                        // BOTTOMLEFT
+                        projectileSpawn.transform.position = new Vector3(transform.position.x - 1, transform.position.y - 1, transform.position.z);
+                        facing = Direction.BottomLeft;
+                    }
+                    else
+                    {
+                        // LEFT
+                        projectileSpawn.transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+                        facing = Direction.Left;
+                    }
+                }
             }
             else
             {
-                projectileSpawn.transform.position = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
-                facing = Direction.Left;
+                if (mousePos.y > transform.position.y + 0.5)
+                {
+                    // TOP
+                    projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+                    facing = Direction.Top;
+                }
+                else
+                {
+                    //BOTTOM
+                    projectileSpawn.transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+                    facing = Direction.Bottom;
+                }
             }
+        }
+
+        if (facing == Direction.BottomRight || facing == Direction.Right || facing == Direction.TopRight)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
         }
 
         // PROJECTILE 
@@ -153,7 +191,7 @@ public class PlayerController : NetworkBehaviour
         obj2Bdestroyed = Instantiate(projectilePrefab, projectileSpawnPos, transform.rotation);
         heading = mouseTarget - new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
         direction = heading / heading.magnitude;
-        obj2Bdestroyed.GetComponent<Rigidbody2D>().velocity = direction * 10;
+        obj2Bdestroyed.GetComponent<Rigidbody2D>().velocity = direction * 30;
 
         // Spawn the bullet on the Clients
         NetworkServer.Spawn(obj2Bdestroyed);
@@ -167,25 +205,45 @@ public class PlayerController : NetworkBehaviour
     void Cmdstep()
     {
         var stepTime = 50;
-        if (facing == Direction.Top)
+        var stepTime2 = 80;
+
+        switch (facing)
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Time.deltaTime * stepTime);
-            targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
-        }
-        if (facing == Direction.Right)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Time.deltaTime * stepTime);
-            targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
-        }
-        if (facing == Direction.Bottom)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Time.deltaTime * stepTime);
-            targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
-        }
-        if (facing == Direction.Left)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), Time.deltaTime * stepTime);
-            targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+            case Direction.Top:
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Time.deltaTime * stepTime);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+
+            case Direction.Right:
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Time.deltaTime * stepTime);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+            case Direction.Bottom:
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Time.deltaTime * stepTime);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+            case Direction.Left:
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), Time.deltaTime * stepTime);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+
+            // DIAGONAL
+            case Direction.TopRight:
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + 1, transform.position.y + 1, transform.position.z), Time.deltaTime * stepTime2);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+            case Direction.TopLeft:
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - 1, transform.position.y + 1, transform.position.z), Time.deltaTime * stepTime2);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+            case Direction.BottomRight:
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + 1, transform.position.y - 1, transform.position.z), Time.deltaTime * stepTime2);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
+            case Direction.BottomLeft:
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - 1, transform.position.y - 1, transform.position.z), Time.deltaTime * stepTime2);
+                targetPosition = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+                break;
         }
     }
 }
