935180dc3c080004851ef438bd04420944a767ec
GPH <gph1990@live.com>
2018-07-18 00:08:32 -0300

    new 3d proj

---
+++ Assets/scripts/_player_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class _player_movement : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(mousePos);
+        }
+
+    }
+
+}
